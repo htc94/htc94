e417f3e85eb5fec3cff1568ebdf800c3e54bc153
Your Name <moonshine_htc@hotmail.com>
2011-05-04 15:57:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Htc94::Application.config.secret_token = '2ea3af6156b414fec1ed0084e1a94368962609942caf2f1dcb284440b17431086d66259828264d2fa5c28a7cc6f3b1e4c01d2ab25d67a43f0863a7a6dec29966'
